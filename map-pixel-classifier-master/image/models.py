8f4d34fa244016220b47140f19076443607c86c9
Daniel Martinez <d.martinez74@gmail.com>
2021-03-03 15:48:11 -0500

    ML Map Classifier
    ML Classifier for Masters Degree

---
+++ map-pixel-classifier-master/image/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.conf import settings
+
+import os 
+
+# Create your models here.
+
+class SImage(models.Model):
+
+    NN_1 = '1'
+    NN_2 = '2'
+    NB_1 = '3'
+    NB_2 = '4'
+
+    MODEL_SELECT = (
+        (NN_1, "Red Neuronal (Sin nubes)"),
+        # (NN_2, "Red Neuronal 2"),
+        (NB_1, "Naive Bayes (Multinomial)"),
+        (NB_2, "Naive Bayes (Gausiano)")
+    )
+
+    WAITING = '0'
+    PROCESSING = '1'
+    PROCESSED =  '2'
+    ERROR = '3'
+
+    PROCESSING_STATUS = (
+        (WAITING,'Waiting'),
+        (PROCESSING,'Processing'),
+        (PROCESSED,'Processed'),
+        (ERROR,'Error')
+    )
+
+    IDEAM = '0'
+    USGS = '1'
+
+    SOURCE = (
+        (IDEAM,'Colombia Geoscience Datacube IDEAM'),
+        (USGS,'United States Geological Survey USGS')
+    )
+
+
+    # file will be saved to MEDIA_ROOT/uploads/2015/01/30
+    upload = models.FileField(upload_to='uploads/')
+    model = models.CharField(max_length=2,choices=MODEL_SELECT,default=NN_1)
+    source = models.CharField(max_length=2,choices=SOURCE,default=USGS)
+    status = models.CharField(max_length=2,choices=PROCESSING_STATUS,default=WAITING)
+    north = models.CharField(max_length=200)
+    south = models.CharField(max_length=200)
+    east = models.CharField(max_length=200)
+    west = models.CharField(max_length=200)
+
+    def processed_path(self):
+        return self.upload.name[:-4] + '_processed.png'
+
+    def processed_url(self):
+        return os.path.join(settings.MEDIA_URL,self.processed_path())
---
+++ map-pixel-classifier-master/image/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, reverse
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from image.models import SImage
+from django.conf import settings
+
+# Create your views here.
+
+from django.views.generic.base import TemplateView
+
+from image.tasks import start_image_processing_nn_1
+from image.tasks import start_image_processing_nb_1
+from image.tasks import start_image_processing_nb_2
+
+import os
+
+class IndexView(TemplateView):
+
+    def get(self,request,*args,**kwargs):
+        template_name = 'image/index.html'
+        return render(request,template_name)
+
+
+class MapView(TemplateView):
+
+    def get(self,request,*args,**kwargs):
+        template_name = 'image/map.html'
+        return render(request,template_name)
+
+class UploadImage(CreateView):
+    # Use simage_form.html as template implicity
+    model = SImage
+    fields = ['upload','model']
+
+    def get_success_url(self):
+        # return reverse('image:index', kwargs={'pk': self.object.pk})
+        
+        image_data = {
+            'id': self.object.id,
+            'file_url': os.path.join(settings.MEDIA_ROOT,self.object.upload.name),
+            'classified_path': os.path.join(settings.MEDIA_ROOT,self.object.processed_path()) 
+        }
+
+        db_data = {
+            'database_name': settings.DATABASES['default']['NAME']
+        }
+
+        if self.object.model == SImage.NB_1:
+            model_data = {
+                'path': os.path.join(settings.MEDIA_ROOT,'model_nb_multi_nubes_1.joblib')
+            }
+            start_image_processing_nb_1.delay(image_data,model_data,db_data)
+        elif self.object.model == SImage.NB_2:
+            model_data = {
+                'path': os.path.join(settings.MEDIA_ROOT,'model_nb_gausiano_nubes_2.joblib')
+            }
+            start_image_processing_nb_2.delay(image_data,model_data,db_data)
+        else:
+            model_data = {
+                'path': os.path.join(settings.MEDIA_ROOT,'model_nn_1.h5')
+            }
+            start_image_processing_nn_1.delay(image_data,model_data,db_data)
+
+
+        return reverse('image:list')
+
+class ListImages(ListView):
+    # Use simage_list.html as template implicity
+    model = SImage
+
+
+class DetailImage(DetailView):
+    # Use simage_detail.html as template implicity
+    model = SImage
